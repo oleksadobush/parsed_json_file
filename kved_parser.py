f5d0026857ad4b8fe3266f02c7c090fc836ac3a0
oleksadobush <oleksandra.stasiuk@ucu.edu.ua>
2021-02-21 20:04:43 +0200

    added final file and changes

--- kved_parser.py
+++ kved_parser.py
@@ -1,36 +1,52 @@
+"""Thid module parses json file for comfortable usage"""
 import json
+import doctest
 
 
 def parse_kved(class_code):
     """
-
+    Parses json file
     :param class_code:
     :return:
+    >>> print(parse_kved(None))
+    None
     """
-    pass
+    if class_code is None:
+        return None
+    dictionary = find_json_info(class_code)
+    with open("kved_results.json", 'w') as outfile:
+        json.dump(dictionary, outfile, ensure_ascii=False)
 
 
 def convert_json(file):
     """
-
+    Reads json file and converts it into dictionary
     :param file:
     :return:
+    >>> print(convert_json(None))
+    None
     """
+    if file is None:
+        return None
     with open(file, encoding='utf-8', errors='ignore') as json_data:
-        into = json.load(json_data, strict=False)
-    return into
+        return json.load(json_data, strict=False)
 
 
 def find_section_code(class_code):
     """
-
+    Returns section code for class code in json file
     :param class_code:
     :return:
+    >>> find_section_code('01.11')
+    'A'
     """
     indicator = int(class_code[:2])
-    dict_codes = {(1, 5): 'A', (5, 10): 'B', (10, 35): 'C', (35, 36): 'D', (36, 41): 'E', (41, 45): 'F',
-                  (45, 49): 'G', (49, 55): 'H', (55, 58): 'I', (58, 64): 'J', (64, 68): 'K', (68, 69): 'L',
-                  (69, 77): 'M', (77, 84): 'N', (84, 85): 'O', (85, 86): 'P', (86, 90): 'Q', (90, 94): 'R',
+    dict_codes = {(1, 5): 'A', (5, 10): 'B', (10, 35): 'C',
+                  (35, 36): 'D', (36, 41): 'E', (41, 45): 'F',
+                  (45, 49): 'G', (49, 55): 'H', (55, 58): 'I',
+                  (58, 64): 'J', (64, 68): 'K', (68, 69): 'L',
+                  (69, 77): 'M', (77, 84): 'N', (84, 85): 'O',
+                  (85, 86): 'P', (86, 90): 'Q', (90, 94): 'R',
                   (94, 97): 'S', (97, 99): 'T', (99, 100): 'U'}
     for key in dict_codes:
         start, end = key
@@ -38,12 +54,16 @@ def find_section_code(class_code):
             return dict_codes[key]
 
 
-def find_class(class_code):
+def find_json_info(class_code):
     """
-
+    Returns desirable info from json file
     :param class_code:
     :return:
+    >>> print(find_json_info(None))
+    None
     """
+    if class_code is None:
+        return None
     dict_json = convert_json('kved.json')
     dict_results = {"type": "class",
                     "parent": {"type": "group",
@@ -76,12 +96,4 @@ def find_class(class_code):
                                     dict_results["name"] = class_name
                                     return dict_results
 
-
-def save_new_json():
-    """
-
-    :return:
-    """
-    pass
-
-# pprint(convert_json("kved.json"))
+doctest.testmod()
